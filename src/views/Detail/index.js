d98a6156d0d485887a9e47ff3e398dc18030425d
Joao Alves <joaol.alves@gmail.com>
2021-01-10 14:25:04 -0300

    styling at hero description

--- src/views/Detail/index.js
+++ src/views/Detail/index.js
@@ -7,6 +7,8 @@ import PageTitle from '../../components/PageTitle';
 import Header from '../../components/Header';
 import Mock from './mockData.json';
 import Comics from './comicsInfoMock.json';
+import { ReactComponent as ChevronLeft } from '../../assets/fi_chevron-left.svg';
+import styles from './styles.module.scss';
 
 const Detail = () => {
   const [heroInfo, setHeroInfo] = useState(Mock.results[0]);
@@ -31,34 +33,52 @@ const Detail = () => {
     <>
       <PageTitle title={`Marvel - ${heroInfo.name}`} />
       <Header color="#FFF" />
-      <h1>{heroInfo.name}</h1>
-      <img
-        src={`${heroInfo.thumbnail.path}/portrait_uncanny.jpg`}
-        alt={`${heroInfo.name} portrait`}
-      />
-      <button onClick={history.goBack}>Voltar</button>
-      <section>
-        {heroInfo.description.length > 1 ? (
-          <p>{heroInfo.description}</p>
-        ) : (
-          <p>Sem descrição disponível</p>
-        )}
-      </section>
-      <section>
-        <h2>
-          HQs que <strong>{heroInfo.name}</strong> aparece
-        </h2>
-        {comicsInfo &&
-          comicsInfo.map((comic, index) => (
-            <div key={index}>
-              <img
-                src={`${comic.thumbnail.path}/portrait_uncanny.jpg`}
-                alt="comic cover"
-              />
-              <h3>{comic.title}</h3>
+      <div className={styles.detailsBackground}>
+        <section className={styles.description}>
+          <button
+            className={styles.goBackButton}
+            onClick={history.goBack}
+          >
+            <ChevronLeft />
+          </button>
+          <figure
+            className={styles.heroPortrait}
+            style={{
+              backgroundImage: `url(${heroInfo.thumbnail.path}/portrait_uncanny.jpg)`
+            }}
+          >
+            <img
+              src={`${heroInfo.thumbnail.path}/portrait_uncanny.jpg`}
+              alt={`${heroInfo.name} portrait`}
+            />
+          </figure>
+          <section>
+            <div className={styles.heroName}>
+              <h1>{heroInfo.name}</h1>
             </div>
-          ))}
-      </section>
+            {heroInfo.description.length > 1 ? (
+              <p>{heroInfo.description}</p>
+            ) : (
+              <p>Sem descrição disponível</p>
+            )}
+          </section>
+        </section>
+        <section>
+          <h2>
+            HQs que <strong>{heroInfo.name}</strong> aparece
+          </h2>
+          {comicsInfo &&
+            comicsInfo.map((comic, index) => (
+              <div key={index}>
+                <img
+                  src={`${comic.thumbnail.path}/portrait_uncanny.jpg`}
+                  alt="comic cover"
+                />
+                <h3>{comic.title}</h3>
+              </div>
+            ))}
+        </section>
+      </div>
     </>
   );
 };
