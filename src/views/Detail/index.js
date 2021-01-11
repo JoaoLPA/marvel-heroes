7172c5351cdebe9cfa0910549ae33ea2509ee587
Joao Alves <joaol.alves@gmail.com>
2021-01-11 01:22:55 -0300

    deploy fixes

--- src/views/Detail/index.js
+++ src/views/Detail/index.js
@@ -12,8 +12,6 @@ import { ReactComponent as ChevronLeft } from '../../assets/fi_chevron-left.svg'
 import { ReactComponent as Book } from '../../assets/fi_book-open.svg';
 import styles from './styles.module.scss';
 
-import ComicsInfoMock from './comicsInfoMock.json';
-
 const Detail = ({ location }) => {
   const { loading, setLoading } = useContext(GlobalContext);
 
